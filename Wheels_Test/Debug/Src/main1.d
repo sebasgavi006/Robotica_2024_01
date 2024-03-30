f7cd1354a8265a1004b672382b04071882a5e5fc
sebasgavi006 <sgaviriav@unal.edu.co>
2024-03-29 22:55:25 -0500

    Wheels_Test actual file

---
+++ Wheels_Test/Debug/Src/main1.d
@@ -0,0 +1,33 @@
+Src/main1.o: ../Src/main1.c \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/core_cm4.h \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/gpio_driver_hal.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/timer_driver_hal.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/usart_driver_hal.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/pwm_driver_hal.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/exti_driver_hal.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/gpio_driver_hal.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/systick_driver_hal.h \
+ /home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/pll_driver_hal.h
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/core_cm4.h:
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/cmsis_version.h:
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Include/mpu_armv7.h:
+/home/sebas/STM32Cube/Robotics/workspace_fundamentos_robotica/CMSIS-repo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/gpio_driver_hal.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/timer_driver_hal.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/usart_driver_hal.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/pwm_driver_hal.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/exti_driver_hal.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/gpio_driver_hal.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/systick_driver_hal.h:
+/home/sebas/Repositories/GitHub/sgaviriav_Robotica/PeripheralsDrivers/Inc/pll_driver_hal.h:
--- Wheels_Test/Src/main1.c
+++ Wheels_Test/Src/main1.c
@@ -16,6 +16,8 @@
  ******************************************************************************
  */
 #include <stdint.h>
+#include "string.h"
+#include <stdio.h>
 #include <math.h>
 #include "stm32f4xx.h"
 //#include "stm32_assert.h"
@@ -28,10 +30,10 @@
 #include "pll_driver_hal.h"
 
 
-#include "string.h"
 
 // Se define un pin para el Blinky
 GPIO_Handler_t stateLed = {0}; // PinA5
+GPIO_Handler_t stateLedBoard = {0}; // PinA5
 
 // Pines para el motor Derecho
 GPIO_Handler_t GPIO_Enb_R = {0};
@@ -74,22 +76,41 @@ uint16_t counter_L = 0;
 //USART
 GPIO_Handler_t handlerPinTX		= {0};
 GPIO_Handler_t handlerPinRX		= {0};
-USART_Handler_t usart2Comm		= {0};
+USART_Handler_t usart1Comm		= {0};
 
 
-char bufferMsg[64] = {0};
+char bufferMsg[128] = {0};
+char bufferReceiver[64] = {0};
 uint8_t rxData = 0;
+uint8_t counterReception = 0;
+uint8_t stringComplete = 0;
 
+// Variables para los comandos
+char cmd[128] = {0};
+unsigned int firstParameter = 0;
+unsigned int secondParameter = 0;
+char lastString[32] = {0};
+
+// Variables globales para el funcionamiento del robot
+uint8_t defaultSpeed = 0;
+uint8_t counterPeriod = 0;
+uint8_t flagPeriod = 0;
+uint8_t counterPercDuty = 0;
+uint8_t flagEncR = 0;
+uint8_t flagEncL = 0;
+uint8_t flagStop = 0;
 
 // Funciones privadas
 void initSystem(void);
 void forwardMove(uint8_t dutyPercentage);
 void backwardMove(uint8_t dutyPercentage);
 void parseCommands(char  *ptrbufferReception);
+void turnOff(void);
+void turnOn(void);
 
-
-/* ===== Funciión principal del programa ===== */
+/* ===== Función principal del programa ===== */
 int main(void){
+
 
 	// Configuramos el PLL para que el micro corra a 100MHz
 	systemClock_100MHz(&pllHandler);
@@ -99,49 +120,51 @@ int main(void){
 	RCC->CR |= (11 << RCC_CR_HSITRIM_Pos); // Numero para calibrar POR DEFECTO ESTABA EN 15!!!!!
 
 	initSystem();
-
-	/* Activar motor derecho */
-
-	// Encendemos el puente H
-	gpio_WritePin(&GPIO_Enb_R, RESET);	// Prende motor derecho
-	gpio_WritePin(&GPIO_Enb_L, RESET);	// Apaga motor izquierdo
-
-	// Seteamos la dirección del motor
-	gpio_WritePin(&GPIO_Dir_R, SET);
-	gpio_WritePin(&GPIO_Dir_L, RESET);
-
-
-	PWM_R.config.polarity	= PWM_POLARITY_DIRECT;
-	pwm_Config(&PWM_R);
-	selectPolarity(&PWM_R);
-
-
-	PWM_L.config.polarity	= PWM_POLARITY_INVERSE;
-	pwm_Config(&PWM_L);
-	selectPolarity(&PWM_L);
-
-	stopPwmSignal(&PWM_R);
-	stopPwmSignal(&PWM_L);
-
-
-	// Encendemos el PWM para mover el motor derecho
-	startPwmSignal(&PWM_R);
-	startPwmSignal(&PWM_L);
-	systick_Delay_ms(SYSTICK_10s);
-	updateDutyCycle(&PWM_R, 90);
-
-
-	// Ponemos un delay de 10s
-	//systick_Delay_ms(SYSTICK_10s);
-
-	// Detenemos el PWM
-	//stopPwmSignal(&PWM_R);
+	turnOff();
+//	systick_Delay_ms(SYSTICK_10s);
+	sprintf(bufferMsg, "Saludos terrícolas, soy OPPY \n");
+	usart_WriteMsg(&usart1Comm, bufferMsg);
+//	forwardMove(1);
+	counterPeriod = 0;
+	counterPercDuty = 0;
+	flagEncR = 0;
+	flagEncL = 0;
 
 
 	/* Loop forever */
 	while(1){
 
+
+		// Se revisa cual fue el dato recibido por la comunicacion serial
+		if(rxData != '\0'){
+			bufferReceiver[counterReception] = rxData;
+			counterReception++;
+
+			// Se verifica si el último dato es el caracter de finalizacion de un string
+			if (rxData == '@'){
+				// Se modifica el estado de una variable de control
+				stringComplete = 1;
+
+				// Configuramos las variables para guardar el string y esperar uno nuevo
+				bufferReceiver[counterReception] = '\0';
+				counterReception = 0;
+			}
+
+			// Limpiamos la variable que almacena los datos recibidos por el com. serial
+			rxData = '\0';
 		}
+		else{
+			// Aun no se han recibido caractéres a traves de la comunicacion serial
+			stringComplete = 0;
+		}
+
+
+		if(stringComplete){
+			parseCommands(bufferReceiver);
+			stringComplete = 0;
+		}
+
+	}
 
 
 }	// Fin del main
@@ -160,7 +183,15 @@ void initSystem(void){
 	stateLed.pinConfig.GPIO_PinOutputSpeed		= GPIO_OSPEED_MEDIUM;
 	stateLed.pinConfig.GPIO_PinPuPdControl		= GPIO_PUPDR_NOTHING;
 	gpio_Config(&stateLed);
-	gpio_WritePin(&stateLed, SET);
+
+	/* Configurando el pin para el Blinky */
+	stateLedBoard.pGPIOx							= GPIOC;
+	stateLedBoard.pinConfig.GPIO_PinNumber			= PIN_5;	// PinA5
+	stateLedBoard.pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	stateLedBoard.pinConfig.GPIO_PinOutputType		= GPIO_OTYPE_PUSHPULL;
+	stateLedBoard.pinConfig.GPIO_PinOutputSpeed		= GPIO_OSPEED_MEDIUM;
+	stateLedBoard.pinConfig.GPIO_PinPuPdControl		= GPIO_PUPDR_NOTHING;
+	gpio_Config(&stateLedBoard);
 
 	/* Configurando el pin para el Enable del puente H */
 	GPIO_Enb_R.pGPIOx							= GPIOC;
@@ -246,16 +277,16 @@ void initSystem(void){
 	PWM_R.ptrTIMx					= TIM5; // Timer5 usado para el PWM
 	PWM_R.config.channel			= PWM_CHANNEL_1;
 	PWM_R.config.prescaler			= 100E3; 	// 1 ms
-	PWM_R.config.periodo			= 500;		// 500 ms
-	PWM_R.config.percDuty			= 10;		// Activo por 5 ms
+	PWM_R.config.periodo			= 100;		// 500 ms
+	PWM_R.config.percDuty			= 0;		// Activo por 5 ms
 	pwm_Config(&PWM_R);
 
 	/* Configurando el PWM para el motor IZQUIERDO */
-	PWM_L.ptrTIMx					= TIM5; // Timer3 usado para el PWM
+	PWM_L.ptrTIMx					= TIM5; // Timer5 usado para el PWM
 	PWM_L.config.channel			= PWM_CHANNEL_2;
 	PWM_L.config.prescaler			= 100E3; 	// 1 ms
-	PWM_L.config.periodo			= 500;		// 500 ms
-	PWM_L.config.percDuty			= 10;		// Activo por 5 ms
+	PWM_L.config.periodo			= 100;		// 500 ms
+	PWM_L.config.percDuty			= 0;		// Activo por 5 ms
 	pwm_Config(&PWM_L);
 
 
@@ -272,55 +303,47 @@ void initSystem(void){
 
 	// 5. ====== SYSTICK =====
 	SystickHandler.pSystick 						= SYSTICK;
-	SystickHandler.Systick_Config.Systick_Reload	= 100000;
-	SystickHandler.Systick_Config.Systick_IntState 	= SYSTICK_ON;
+	SystickHandler.Systick_Config.Systick_Reload	= SYSTICK_PSC_1ms_100MHz;
+	SystickHandler.Systick_Config.Systick_IntState 	= SYSTICK_INT_ENABLE;
 	systick_Config(&SystickHandler);
+	systick_SetState(&SystickHandler, SYSTICK_ON);
+
 
 	/* ==================================== Configurando los USART =============================================*/
-	handlerPinTX.pGPIOx												= GPIOA;
-	handlerPinTX.pinConfig.GPIO_PinNumber					= PIN_2;
+	handlerPinTX.pGPIOx										= GPIOA;
+	handlerPinTX.pinConfig.GPIO_PinNumber					= PIN_9;
 	handlerPinTX.pinConfig.GPIO_PinMode						= GPIO_MODE_ALTFN;
-	handlerPinTX.pinConfig.GPIO_PinAltFunMode					= AF7;
+	handlerPinTX.pinConfig.GPIO_PinAltFunMode				= AF7;
 	gpio_Config(&handlerPinTX);
 
-	handlerPinRX.pGPIOx												= GPIOA;
-	handlerPinRX.pinConfig.GPIO_PinNumber						= PIN_3;
+	handlerPinRX.pGPIOx										= GPIOA;
+	handlerPinRX.pinConfig.GPIO_PinNumber					= PIN_10;
 	handlerPinRX.pinConfig.GPIO_PinMode						= GPIO_MODE_ALTFN;
-	handlerPinRX.pinConfig.GPIO_PinAltFunMode					= AF7;
+	handlerPinRX.pinConfig.GPIO_PinAltFunMode				= AF7;
 	gpio_Config(&handlerPinRX);
 
-	usart2Comm.ptrUSARTx									= USART2;
-	usart2Comm.USART_Config.baudrate						= USART_BAUDRATE_115200;
-	usart2Comm.USART_Config.datasize						= USART_DATASIZE_8BIT;
-	usart2Comm.USART_Config.parity							= USART_PARITY_NONE;
-	usart2Comm.USART_Config.stopbits						= USART_STOPBIT_1;
-	usart2Comm.USART_Config.mode							= USART_MODE_RXTX;
-	usart2Comm.USART_Config.enableIntRX						= USART_RX_INTERRUPT_ENABLE;
-	usart2Comm.USART_Config.enableIntTX						= USART_TX_INTERRUPT_DISABLE;
-	usart_Config(&usart2Comm);
+	usart1Comm.ptrUSARTx									= USART1;
+	usart1Comm.USART_Config.baudrate						= USART_BAUDRATE_19200_100MHz;
+	usart1Comm.USART_Config.datasize						= USART_DATASIZE_8BIT;
+	usart1Comm.USART_Config.parity							= USART_PARITY_NONE;
+	usart1Comm.USART_Config.stopbits						= USART_STOPBIT_1;
+	usart1Comm.USART_Config.mode							= USART_MODE_RXTX;
+	usart1Comm.USART_Config.enableIntRX						= USART_RX_INTERRUPT_ENABLE;
+	usart1Comm.USART_Config.enableIntTX						= USART_TX_INTERRUPT_DISABLE;
+	usart_Config(&usart1Comm);
 
 
 }
 
-
-
-/* Función callback que atiende la interrupción del EXTI8, en este caso, corresponde
- * a la interrupcón externa del encoder
- * */
-void callback_ExtInt1(void){
-	counter_R ++;
-}
-
-/* Callback de Timer 2 para el Blinky */
-void Timer2_Callback(void){
-	gpio_TooglePin(&stateLed);
-	counter_L++;
-}
 
 
 // Función para mover hacia adelante
 
 void forwardMove(uint8_t dutyPercentage){
+
+	stopPwmSignal(&PWM_R);
+	stopPwmSignal(&PWM_L);
+
 	// Encendemos el puente H
 	gpio_WritePin(&GPIO_Enb_R, RESET);	// Prende motor derecho
 	gpio_WritePin(&GPIO_Enb_L, RESET);	// Apaga motor izquierdo
@@ -328,38 +351,6 @@ void forwardMove(uint8_t dutyPercentage){
 	// Seteamos la dirección del motor
 	gpio_WritePin(&GPIO_Dir_R, SET);
 	gpio_WritePin(&GPIO_Dir_L, RESET);
-
-
-	PWM_R.config.polarity	= PWM_POLARITY_DIRECT;
-	pwm_Config(&PWM_R);
-	selectPolarity(&PWM_R);
-
-
-	PWM_L.config.polarity	= PWM_POLARITY_INVERSE;
-	pwm_Config(&PWM_L);
-	selectPolarity(&PWM_L);
-
-	stopPwmSignal(&PWM_R);
-	stopPwmSignal(&PWM_L);
-
-
-	// Encendemos el PWM para mover el motor derecho
-	startPwmSignal(&PWM_R);
-	startPwmSignal(&PWM_L);
-//	systick_Delay_ms(SYSTICK_10s);
-	updateDutyCycle(&PWM_R,dutyPercentage);
-
-}
-
-
-void backwardMove(uint8_t dutyPercentage){
-	// Encendemos el puente H
-	gpio_WritePin(&GPIO_Enb_R, RESET);	// Prende motor derecho
-	gpio_WritePin(&GPIO_Enb_L, RESET);	// Apaga motor izquierdo
-
-	// Seteamos la dirección del motor
-	gpio_WritePin(&GPIO_Dir_R, RESET);
-	gpio_WritePin(&GPIO_Dir_L, SET);
 
 
 	PWM_R.config.polarity	= PWM_POLARITY_INVERSE;
@@ -371,70 +362,256 @@ void backwardMove(uint8_t dutyPercentage){
 	pwm_Config(&PWM_L);
 	selectPolarity(&PWM_L);
 
-	stopPwmSignal(&PWM_R);
-	stopPwmSignal(&PWM_L);
-
+	updateDutyCycle(&PWM_R,dutyPercentage);
+	updateDutyCycle(&PWM_L,dutyPercentage);
 
 	// Encendemos el PWM para mover el motor derecho
 	startPwmSignal(&PWM_R);
 	startPwmSignal(&PWM_L);
-//	systick_Delay_ms(SYSTICK_10s);
+
+}
+
+
+void backwardMove(uint8_t dutyPercentage){
+
+	stopPwmSignal(&PWM_R);
+	stopPwmSignal(&PWM_L);
+
+	// Encendemos el puente H
+	gpio_WritePin(&GPIO_Enb_R, RESET);	// Prende motor derecho
+	gpio_WritePin(&GPIO_Enb_L, RESET);	// Apaga motor izquierdo
+
+	// Seteamos la dirección del motor
+	gpio_WritePin(&GPIO_Dir_R, RESET);
+	gpio_WritePin(&GPIO_Dir_L, SET);
+
+
+	PWM_R.config.polarity	= PWM_POLARITY_DIRECT;
+	pwm_Config(&PWM_R);
+	selectPolarity(&PWM_R);
+
+
+	PWM_L.config.polarity	= PWM_POLARITY_INVERSE;
+	pwm_Config(&PWM_L);
+	selectPolarity(&PWM_L);
+
 	updateDutyCycle(&PWM_R,dutyPercentage);
+	updateDutyCycle(&PWM_L,dutyPercentage);
+
+	// Encendemos el PWM para mover el motor derecho
+	startPwmSignal(&PWM_R);
+	startPwmSignal(&PWM_L);
+
+}
+
+/*
+ * Funcipon para apagar el sistema
+ */
+void turnOff(void){
+
+	// Apaga el puente H para los motores
+	gpio_WritePin(&GPIO_Enb_R, SET);
+	gpio_WritePin(&GPIO_Enb_L, SET);
+
+	// Apaga los PWM
+	stopPwmSignal(&PWM_R);
+	stopPwmSignal(&PWM_L);
+
+}
+
+/*
+ * Funcipon para encender el sistema
+ */
+void turnOn(void){
+
+	// Enciente el puente H de los motores
+	gpio_WritePin(&GPIO_Enb_R, RESET);
+	gpio_WritePin(&GPIO_Enb_L, RESET);
+
+	// Enciente los PWM
+	startPwmSignal(&PWM_R);
+	startPwmSignal(&PWM_L);
 
 }
 
 void parseCommands(char  *ptrbufferReception){
 
-	sscanf(ptrbufferReception,"%s %f %u %u",cmd,&firstParameter,&secondParameter,&thirdParameter);
+	sscanf(ptrbufferReception,"%s %u %u %s",cmd,&firstParameter,&secondParameter,lastString);
 	//Comando para solicitar ayuda
 	if(strcmp(cmd, "help") == 0){
-		usart_WriteMsg(&usart2Comm, "Help Menu CMDS: \n");
+		usart_WriteMsg(&usart1Comm, "Help Menu CMDS: \n");
+		usart_WriteMsg(&usart1Comm, "1) Dir 0:forw / 1:back ; dutty(\%) \" Dir # # @\" \n");
+		usart_WriteMsg(&usart1Comm, "2) Spd \%leftM 		; \%rightM \" Spd # # @\" \n");
+		usart_WriteMsg(&usart1Comm, "3) Rot 0:left 1:right  ; #turns  \" Rot # # @\" \n");
+		usart_WriteMsg(&usart1Comm, "4) TestEncoders percDuttyCycle:left \" TestEncoders # @\" \n");
 
-	}
-	else if (strcmp(cmd, "Reset") == 0) {
-		usart_WriteMsg(&usart2Comm, "Reinicio del sistema \n");
+		usart_WriteMsg(&usart1Comm, "5) Test 0:left / 1:right; dutty   \" Test # # @\" \n");
+
+
+		usart_WriteMsg(&usart1Comm, "6) Stop \" Stop @\" \n");
+		usart_WriteMsg(&usart1Comm, "7) Resume \" Resume @\" \n");
+
 	}
 
 	else if (strcmp(cmd, "Dir") == 0) {
 
-		if (firstParameter == 1 && secondParameter >0){
-			void forwardMove(uint8_t secondParameter);
+		// firstParameter indica la direccion, secondParameter es el dutyCycle
+		if (firstParameter == 0 && secondParameter >= 0){
+			if (defaultSpeed != 0){
+				forwardMove(secondParameter);
+			}
+			else{
+				forwardMove(10);
+			}
+			usart_WriteMsg(&usart1Comm, "Moviéndose hacia adelante \n");
 		}
-		else if (firstParameter == 0 && secondParameter >0){
-			void backwardMove(uint8_t secondParameter);
+		else if (firstParameter == 1 && secondParameter >= 0){
+			if (defaultSpeed != 0){
+				backwardMove(secondParameter);
+			}
+			else{
+				backwardMove(10);
+			}
+			usart_WriteMsg(&usart1Comm, "Moviéndose hacia atrás \n");
 		}
-
+		defaultSpeed = 0;
 	}
-
 
 	else if(strcmp(cmd, "Spd") == 0) {
 			if (firstParameter > 0) {
 
-				updateDutyCycle(&PWM_R,firstParameter);
-				sprintf(bufferMsg,"Velocidad actualizada %.2f s",firstParameter);
-				usart_WriteMsg(&usart2Comm, bufferMsg);
+				defaultSpeed = firstParameter;
+				updateDutyCycle(&PWM_L,(uint16_t)firstParameter);
+				updateDutyCycle(&PWM_R,(uint16_t)secondParameter);
+
+				sprintf(bufferMsg,"Velocidad actualizada: %u, %u \n",firstParameter, secondParameter);
+				usart_WriteMsg(&usart1Comm, bufferMsg);
 			}
 			else{
-				writeMsg(&usart2Comm, "Porcentaje debe ser positivo.\n Ingresa \"help @\" para ver la lista de comandos.\n");
-
+				usart_WriteMsg(&usart1Comm, "Porcentaje debe ser positivo.\n Ingresa \"help @\" para ver la lista de comandos.\n");
 			}
 	}
 
+	else if(strcmp(cmd, "TestE") == 0) {
+
+		usart_WriteMsg(&usart1Comm, "Iniciando Test Encoders\n");
+		forwardMove(firstParameter);
+
+		rxData = '\0';
+		// Conteo y muestra de las interrupciones del encoder
+		while(rxData == '\0'){
+			if(flagEncR){
+				sprintf(bufferMsg,"Right,%u\n", counter_R);
+				usart_WriteMsg(&usart1Comm, bufferMsg);
+				flagEncR = 0;
+			}
+			if(flagEncL){
+				sprintf(bufferMsg,"Left,%u\n", counter_L);
+				usart_WriteMsg(&usart1Comm, bufferMsg);
+				flagEncL = 0;
+			}
+		}
+	}
+
+	else if(strcmp(cmd, "Test") == 0){
+
+		usart_WriteMsg(&usart1Comm, "Iniciando Test \n");
+		forwardMove(0);
+		counter_R = 0;
+		counter_L = 0;
+		counterPercDuty = 0;
+
+		rxData = '\0';
+		// Código para realizar el estudio del comportamiento de los motores y los encoders
+		while(rxData == '\0'){
+
+			// Conteo y muestra de las interrupciones del encoder
+			if(flagEncR){
+				sprintf(bufferMsg,"\nRight,%u,%u\n",counterPercDuty, counter_R);
+				usart_WriteMsg(&usart1Comm, bufferMsg);
+				flagEncR ^= 1;
+			}
+			if(flagEncL){
+				sprintf(bufferMsg,"\nLeft,%u,%u\n",counterPercDuty, counter_L);
+				usart_WriteMsg(&usart1Comm, bufferMsg);
+				flagEncL ^= 1;
+			}
+
+			// Cada que pase un periodo determinado, el porcentaje del CutyCycle aumenta en 1%
+			if(flagPeriod){
+				counter_R = 0;
+				counter_L = 0;
+				counterPercDuty++;
+				updateDutyCycle(&PWM_R,(uint16_t) counterPercDuty);
+				updateDutyCycle(&PWM_L,(uint16_t) counterPercDuty);
+				flagPeriod ^= 1;
+			}
+			if(counterPercDuty == 99){
+				counterPercDuty = 0;
+				turnOff();
+				usart_WriteMsg(&usart1Comm, "Test finished \n");
+			}
+		}
+
+	}
+
+
+
+
+	else if (strcmp(cmd, "reset") == 0) {
+		usart_WriteMsg(&usart1Comm, "PWR_MGMT_1 reset \n");
+
+	}
+
+	else if (strcmp(cmd, "Stop") == 0) {
+		flagStop = 1;
+		turnOff();
+		usart_WriteMsg(&usart1Comm, "Detiene del sistema \n");
+	}
+
+	else if (strcmp(cmd, "Resume") == 0) {
+		turnOn();
+		usart_WriteMsg(&usart1Comm, "Reanuda del sistema \n");
+	}
 
 	else{
-		writeMsg(&usart2Comm, "Comando erroneo.\n Ingresa \"help @\" para ver la lista de comandos.\n");
+		usart_WriteMsg(&usart1Comm, "Comando erroneo.\n Ingresa \"help @\" para ver la lista de comandos.\n");
+	}
+
+
+}
+
+
+/* Callback de Timer 2 para el Blinky */
+void Timer2_Callback(void){
+	gpio_TooglePin(&stateLed);
+	gpio_TooglePin(&stateLedBoard);
+	counterPeriod++;
+	if(counterPeriod == 10){
+		flagPeriod ^= 1;
+		counterPeriod = 0;
 	}
 }
 
 
-void usart2_RxCallback(void){
-	rxData = usart2_getRxData();
-
-	sprintf(bufferMsg,"Recibido %c \n",rxData);
-	usart_WriteMsg(&usart2Comm, bufferMsg);
-
-
+/* Interrupciones por recepcion a traves de transmision serial */
+void usart1_RxCallback(void){
+	rxData = usart1_getRxData();
 
 }
 
+
+/* Interrupciones para el encoder derecho */
+void callback_ExtInt1(void){
+	flagEncR = 1;
+	counter_R++;
+}
+
+
+/* Interrupciones para el encoder izquierdo */
+void callback_ExtInt3(void){
+	flagEncL = 1;
+	counter_L++;
+
+}
 
